af9e82e9507cf7691f1975f8cdc47a20237520d3
andre <andreluizjp2015@gmail.com>
2025-10-09 17:24:30 -0300

    criacao  componente home

---
+++ src/components/comuns/PesquisarVaga.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="h100 p-5 bg-light border rounded-3">
+
+        <h2>Pesquisar vagas</h2>
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label>Titulo da vaga</label>
+                    <input type="text" class="form-control"
+                        placeholder="pesquisa por palavras chaves, por exemplo 'php', 'Senior', 'DBA'">
+                    <small class="form-text text-muted">Informe palavras que estão relacionadas com o titulo da vaga que
+                        procura.</small>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PesquisarVaga'
+}
+</script>
+
+<style></style>
--- src/components/views/HomePage.vue
+++ src/components/views/HomePage.vue
@@ -1,19 +1,49 @@
 <template>
-    <div>
-        <p>home</p>
+    <div class="container py-4">
+        <div class="row">
+            <div class="col">
+                <pesquisar-vaga />
+            </div>
+        </div>
+        <div class="row mt-5 text-center">
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+                    <p>Vagas abertas</p>
+                    <h2>25</h2>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+                    <p>Prodissionais cadastrados</p>
+                    <h2>125</h2>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="h-100 p-5 rounded-3 border bg-light text-dark">
+                    <p>Visitantes online</p>
+                    <h2>15</h2>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+
+import PesquisarVaga from "@/components/comuns/PesquisarVaga.vue"
+
 export default {
     name: 'HomePage',
+    components: {
+        PesquisarVaga
+    },
     activated() {
         console.log('Componente ativado')
     },
     deactivated() {
         console.log('componente desativado')
     },
-        beforeUnmount() {
+    beforeUnmount() {
         console.log('Antes de desmontar')
     },
     unmounted() {
