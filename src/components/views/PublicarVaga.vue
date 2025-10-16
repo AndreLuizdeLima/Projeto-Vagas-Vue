705f3d17f43486076d693b4056b6c3b08bccf118
Andre <andreluizjp2015@gmail.com>
2025-10-15 22:24:26 -0300

    armanezando as vagas

--- src/components/views/PublicarVaga.vue
+++ src/components/views/PublicarVaga.vue
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="col">
                 <label class="form-label mt-3">Titulo da Vaga</label>
-                <input type="text" class="form-control">
+                <input type="text" class="form-control" v-model="titulo">
                 <div class="form-text">Por exemplo: Programador JavaScript e VueJs</div>
             </div>
         </div>
@@ -17,21 +17,22 @@
         <div class="row">
             <div class="col">
                 <label class="form-label mt-3">Descrição</label>
-                <textarea type="text" class="form-control"></textarea>
+                <textarea type="text" class="form-control" v-model="descricao"></textarea>
                 <div class="form-text">Informe detalhes da vaga</div>
             </div>
         </div>
 
-         <div class="row">
+        <div class="row">
             <div class="col">
                 <label class="form-label mt-3">Salario</label>
-                <input type="number" class="form-control">
+                <input type="number" class="form-control" v-model="salario">
                 <div class="form-text">Informe o salario</div>
             </div>
 
             <div class="col">
                 <label class="form-label mt-3">Modalidade</label>
-                <select class="form-select">
+                <select class="form-select" v-model="modalidade">
+                    <option value="" disabled>Selecione</option>
                     <option value="1">Home Office</option>
                     <option value="2">Presencial</option>
                 </select>
@@ -40,7 +41,8 @@
 
             <div class="col">
                 <label class="form-label mt-3">Tipo</label>
-                <select class="form-select">
+                <select class="form-select" v-model="tipo">
+                    <option value="" disabled>Selecione</option>
                     <option value="1">CLT</option>
                     <option value="2">PJ</option>
                 </select>
@@ -50,7 +52,8 @@
 
         <div class="row mt-3">
             <div class="col">
-                <button type="submit" class="btn btn-primary">Cadastrar</button>
+                <button type="submit" class="btn btn-primary bg-black border-black"
+                    @click="salvarVaga()">Cadastrar</button>
             </div>
         </div>
     </div>
@@ -58,7 +61,33 @@
 
 <script>
 export default {
-    name: 'PublicarVaga'
+    name: 'PublicarVaga',
+    data: () => ({
+        titulo: '',
+        descricao: '',
+        salario: '',
+        modalidade: '',
+        tipo: ''
+    }),
+    methods: {
+        salvarVaga() {
+            let vagas = JSON.parse(localStorage.getItem('vagas'))   
+
+            if (!vagas) vagas = []
+
+            vagas.push(
+                {
+                    titulo: this.titulo,
+                    descricao: this.descricao,
+                    salario: this.salario,
+                    modalidade: this.modalidade,
+                    tipo: this.tipo,
+                }
+            )
+
+            localStorage.setItem('vagas', JSON.stringify(vagas))
+        }
+    }
 }
 </script>
 
